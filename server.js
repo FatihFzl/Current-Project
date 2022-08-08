1f554bd8813f80d5d74f5691e6b373994f08152d
FatihFzl <108987129+FatihFzl@users.noreply.github.com>
2022-08-08 14:57:46 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const {
   GraphQLInt,
   GraphQLNonNull
 } = require('graphql')
-const app = express()
+
+//const app = express()
 
 const authors = [
 	{ id: 1, name: 'J. K. Rowling' },
@@ -133,3 +134,60 @@ app.use('/graphql', expressGraphQL({
   graphiql: true
 }))
 app.listen(5000, () => console.log('Server Running'))
+
+
+const express = require('express')
+const app = express()
+const fileUpload = require('express-fileupload');
+
+const mongoose = require('mongoose');
+
+app.use(express.json())
+
+mongoose.connect(process.env.MONGODB_URL).catch(err => {
+    console.error(err);
+})
+
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(logger('dev'));
+
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 5000}));
+app.use(bodyParser.json({limit: '50mb'}));
+
+//Graphql server
+
+app.use('/graphql', jwt({
+    secret: process.env.JWT_SECRET_KEY,
+    requestProperty: 'auth',
+    credentialsRequired: false,
+}));
+
+//graphql setting
+
+app.use('/graphql', async (req, res, done) => {
+    var userId = (req.auth && req.auth.id ) ? req.auth.id : undefined;
+    const user = ( userId ) ? await User.findById(userId): undefined;
+    req.context = {
+        user: user,
+    }
+    done();
+});
+app.use('/graphql', UploadProfilePicture);
+app.use('/graphql', expressGraphQL(req => ({
+        schema: GraphQLSchema,
+        context: req.context,
+        graphiql: process.env.NODE_ENV === 'development',
+    })
+));
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+});
+
+module.exports = app;
